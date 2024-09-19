748fba76bbd6b8af702c8e0fa3619e925924201b
Yoann Auroy <yoann.ar.pro@gmail.com>
2024-09-19 09:28:55 +0200

    adding tests files

---
+++ src/Components/CategoryPage/CategoryPage.test.ts
@@ -0,0 +1,23 @@
+// test("", () => {
+//   expect().toBe();
+// });
+
+/**
+ *  const findCategory = () => {
+      if (categories.length > 0) {
+        const findedCategory = categories.find(
+          (category) => category.slug === params.slug,
+        );
+
+        if (!findedCategory) return;
+
+        const filteredProducts = products?.filter(
+          (product) => product.categoryId === findedCategory?.id,
+        );
+        if (filteredProducts.length > 0)
+          setCategoryProducList(filteredProducts);
+
+        return setCurrentCategory(findedCategory);
+      }
+    };
+ */
