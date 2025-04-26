4eea5baf91737293be07c1788f919fe0fe40830c
Yoann Auroy <yoann.ar.pro@gmail.com>
2025-04-26 13:45:18 +0200

    create useRemoveFromCart hook

---
+++ src/hooks/useRemoveFromCart.ts
@@ -0,0 +1,57 @@
+import { useSelector } from "react-redux";
+import { useMutation } from "@tanstack/react-query";
+
+import type { RootState } from "store/store";
+import type { IProductCart, ICart, ICartListItem } from "types";
+import { axiosInstance } from "infrastructure/api/axios";
+
+export const useRemoveFromCart = () => {
+  //todo : add toast notification for success and error
+
+  const { mutate: syncCart } = useMutation({
+    mutationFn: async ({ user_id, cart }: ICart) => {
+      const result = await axiosInstance.put("cart", { user_id, cart });
+      return result.data;
+    },
+    onSuccess: () => {},
+    onError: () => {
+      alert("Erreur d'ajout au panier");
+    },
+  });
+  const user_id = useSelector(
+    (state: RootState) => state.appStore.login.user.id,
+  );
+  const is_logged = useSelector((state: RootState) => state.appStore.isLogged);
+
+  return (product: IProductCart) => {
+    try {
+      // local_cart is an array of {"product_id": number,"quantity": number}
+      const local_cart = localStorage.getItem("cart");
+      let cart_list: Array<ICartListItem> = local_cart
+        ? JSON.parse(local_cart)
+        : [];
+      if (local_cart) {
+        const product_in_cart = cart_list.find(
+          (item: ICartListItem) => item.product_id === product.id,
+        );
+        if (product_in_cart && product_in_cart.quantity > 1) {
+          product_in_cart.quantity -= 1;
+        } else if (product_in_cart && product_in_cart.quantity === 1) {
+          product_in_cart.quantity -= 1;
+          cart_list = cart_list.filter(
+            (item: ICartListItem) => item.product_id !== product.id,
+          );
+        }
+      }
+      localStorage.setItem("cart", JSON.stringify(cart_list));
+
+      if (is_logged) {
+        syncCart({ user_id, cart: cart_list });
+      }
+      return true;
+    } catch (error) {
+      alert("Erreur d'ajout au panier");
+      return false;
+    }
+  };
+};
